b7677cc7bf29a6646d53209e007100d0ed6fcf9f
Diones <pereiradiones987@gmail.com>
2023-09-30 16:52:39 -0300

    first commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly courseService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.courseService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.courseService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateCourseDTO) {
+    return this.courseService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body: UpdateCourseDTO) {
+    return this.courseService.update(id, body);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.courseService.remove(id);
+  }
+}
